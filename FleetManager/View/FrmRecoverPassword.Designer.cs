a39d3c5d55de53a1d80bfc96d187d19179f5e6ca
primetek-anderson <anderson.nascimento@primetek.com.br>
2019-11-16 02:14:02 -0300

    Estilização recuperar senha.

--- FleetManager/View/FrmRecoverPassword.Designer.cs
+++ FleetManager/View/FrmRecoverPassword.Designer.cs
@@ -37,22 +37,31 @@
             // lblRecuperar
             // 
             this.lblRecuperar.AutoSize = true;
+            this.lblRecuperar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecuperar.ForeColor = System.Drawing.Color.LightGray;
             this.lblRecuperar.Location = new System.Drawing.Point(48, 36);
             this.lblRecuperar.Name = "lblRecuperar";
-            this.lblRecuperar.Size = new System.Drawing.Size(142, 13);
+            this.lblRecuperar.Size = new System.Drawing.Size(196, 15);
             this.lblRecuperar.TabIndex = 0;
             this.lblRecuperar.Text = "Digite seu Usuario ou E-mail:";
             // 
             // txtUserEmail
             // 
-            this.txtUserEmail.Location = new System.Drawing.Point(131, 52);
+            this.txtUserEmail.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(105)))), ((int)(((byte)(165)))));
+            this.txtUserEmail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUserEmail.ForeColor = System.Drawing.Color.LightGray;
+            this.txtUserEmail.Location = new System.Drawing.Point(131, 62);
             this.txtUserEmail.Name = "txtUserEmail";
-            this.txtUserEmail.Size = new System.Drawing.Size(251, 20);
+            this.txtUserEmail.Size = new System.Drawing.Size(251, 13);
             this.txtUserEmail.TabIndex = 1;
+            this.txtUserEmail.TextChanged += new System.EventHandler(this.txtUserEmail_TextChanged);
+            this.txtUserEmail.MouseLeave += new System.EventHandler(this.txtUserEmail_MouseLeave);
             // 
             // btnEnviar
             // 
             this.btnEnviar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEnviar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviar.ForeColor = System.Drawing.Color.LightGray;
             this.btnEnviar.Location = new System.Drawing.Point(307, 94);
             this.btnEnviar.Name = "btnEnviar";
             this.btnEnviar.Size = new System.Drawing.Size(75, 23);
@@ -64,6 +73,8 @@
             // lblResposta
             // 
             this.lblResposta.AutoSize = true;
+            this.lblResposta.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResposta.ForeColor = System.Drawing.Color.LightGray;
             this.lblResposta.Location = new System.Drawing.Point(48, 134);
             this.lblResposta.Name = "lblResposta";
             this.lblResposta.Size = new System.Drawing.Size(0, 13);
@@ -73,6 +84,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(105)))), ((int)(((byte)(165)))));
             this.ClientSize = new System.Drawing.Size(528, 218);
             this.Controls.Add(this.lblResposta);
             this.Controls.Add(this.btnEnviar);
